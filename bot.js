fd968c97346c69429c7379b699b6bf9a91001f02
рука жопель <cocoin666228337@gmail.com>
2026-05-01 01:40:12 +0300

    remove railway folder

--- bot.js
+++ bot.js
@@ -1,10 +1,12 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
+// данные
 const TELEGRAM_TOKEN = '8687121399:AAFfQZ9NSL00swk76DpMzxzd6jvUhIBvh4I';
 const CHAT_ID = '632786488';
 const GROUP_NAME = 'Барбосы';
 
+// клиент WhatsApp
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
@@ -15,21 +17,23 @@ const client = new Client({
       '--disable-dev-shm-usage',
       '--disable-gpu',
       '--no-first-run',
-      '--no-zygote',
-      '--single-process'
+      '--no-zygote'
     ]
   }
 });
 
+// QR
 client.on('qr', qr => {
   console.log('Сканируй QR:');
   qrcode.generate(qr, { small: true });
 });
 
+// готовность
 client.on('ready', () => {
   console.log('Бот запущен!');
 });
 
+// обработка сообщений
 client.on('message_create', async msg => {
   try {
     const chat = await msg.getChat();
@@ -54,4 +58,14 @@ client.on('message_create', async msg => {
   }
 });
 
+// обработка ошибок клиента
+client.on('auth_failure', msg => {
+  console.error('Ошибка авторизации:', msg);
+});
+
+client.on('disconnected', reason => {
+  console.log('Клиент отключился:', reason);
+});
+
+// запуск
 client.initialize();
